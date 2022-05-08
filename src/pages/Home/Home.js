dda3ee0f4ba4e892636da698594b7b71d91ad9e6
Alex Villecco <amvillecco@teco.com.ar>
2022-05-07 22:27:01 -0300

    Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,32 @@
-import React from "react";
-import './Home.css'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
-
-    return(
-        <div>
-            <h3>
-                Este es el home ! 
-            </h3>
+  return(
+    <>
+      <img style={{ width: 200 }} className="logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="" />
+      <div className="container-nav">
+        <div className="container-btn-nav">
+          <a href="/public/js/login/login.html">
+            <button style={{backgroundColor: "aquamarine"}} className="btn-nav">
+              Ingresá
+            </button>
+          </a>
+          <a href="/public/js/sigin/sigin.html">
+            <button style={{backgroundColor: "#4dff00"}} className="btn-nav">
+              Registrate
+            </button>
+          </a>
         </div>
-    )
+
+        <div className="iconos_nav">
+          <a href=""><i className="fa-brands fa-facebook"></i></a>
+          <a href="https://twitter.com/KaizenZeta"><i className="fa-brands fa-twitter"></i></a>
+          <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Home
+export default Home;
---
+++ src/pages/LoginSuccess/LoginSuccess.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './LoginSuccess.css'
+
+const LoginSuccess = () => {
+
+    return(
+        <div>
+            <h3>
+                ¡Login Exitos!
+            </h3>
+        </div>
+    )
+}
+
+export default LoginSuccess
